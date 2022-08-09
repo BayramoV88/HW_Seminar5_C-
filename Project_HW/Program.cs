a9cea551a7d2fcf0eaa9a1bf4b10a7ca42a733eb
Vadim Bayramov <bayramovva@gmail.com>
2022-08-10 02:25:22 +0300

    Add Zadacha38

--- Project_HW/Program.cs
+++ Project_HW/Program.cs
@@ -18,7 +18,26 @@ void Zadacha36()
     FindSumOddNums(array);
 }
 
-void FillArray(int[] array, int startNum = -20, int finishNum = 20)
+void Zadacha38()
+{
+    int size = 6;
+    double[] array = new double[size];
+    Random rand = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.NextDouble() * 100;
+    }
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(Math.Round(array[i], 2) + ("  "));
+    }
+    Console.WriteLine();
+    Result(array);
+}
+
+void FillArray(int[] array, int startNum = -30, int finishNum = 30)
 {
     Random rand = new Random();
     for (int i = 0; i < array.Length; i++)
@@ -51,14 +70,34 @@ void FindSumOddNums(int [] array)
     int sum = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        if (i % 2 == 1)
-        {
-            sum = sum + array[i];
-        }
+        if (i % 2 == 1) sum = sum + array[i];
     }
     Console.WriteLine($"Сумма элементов на нечетных позициях равна {sum}");
 }
 
+void Result(double[] array)
+{
+    double maxN = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > maxN) maxN = array[i];
+    }
+    Console.WriteLine("Максимальный элемент массива " + (Math.Round(maxN, 2)));
+
+    double minN = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < minN) minN = array[i];
+    }
+    Console.WriteLine("Минимальный элемент массива " + (Math.Round(minN, 2)));
+
+    double result = 0;
+    result = maxN - minN;
+
+    Console.WriteLine("Разница между максимальным и минимальным элементом " + (Math.Round(result, 2)));
+}
+
 
 //Zadacha34();
-Zadacha36();
+//Zadacha36();
+Zadacha38();
