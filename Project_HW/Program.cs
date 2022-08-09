346d71d1080ebb37931fb7f9655cabd18a787af7
Vadim Bayramov <bayramovva@gmail.com>
2022-08-10 00:08:46 +0300

    Add Zadacha34

---
+++ Project_HW/Program.cs
@@ -0,0 +1,41 @@
+﻿void Zadacha34()
+{
+    int size = 10;
+    int[] array = new int[size];
+
+    FillArray(array, 100, 1000);
+    PrintArray(array);
+    FindNums(array);
+}
+
+void FillArray(int[] array, int startNum, int finishNum)
+{
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(startNum, finishNum);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void FindNums(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) sum++;
+    }   
+    Console.WriteLine($"количество четных чисел массива равно {sum}");
+}
+
+
+
+Zadacha34();
